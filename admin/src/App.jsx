bd1873e5b4d923a61803f2a7513e271d41aa2515
david052j <58350244+david052j@users.noreply.github.com>
2024-10-01 14:52:19 +0300

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-del-backend-k23v.onrender.com"
   
   return (
     <div>
